a4ea2c67541dc52351c0accf4d541a66ec769695
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-03 10:54:33 +0700

    #fix labels

--- backend/server.py
+++ backend/server.py
@@ -11,33 +11,48 @@ model = joblib.load('model/model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
+
+    yesOrNoValues = {
+        'yes': 1.0,
+        'no': 0.0,
+    }
+
+    mtransValues = {
+        'Automobile': 0,
+        'Bike': 1,
+        'Motorbike': 2,
+        'Public_Transportation': 3,
+        'Walking': 4,
+    }
+
     pred = model.predict(np.array([[
-        request.json['Age'],
-        request.json['Height'],
-        request.json['Weight'],
-        request.json['family_history'],
-        request.json['FAVC'],
-        request.json['FCVC'],
-        request.json['NCP'],
-        request.json['CAEC'],
-        request.json['SMOKE'],
-        request.json['CH2O'],
-        request.json['SCC'],
-        request.json['FAF'],
-        request.json['TUE'],
-        request.json['CALC'],
-        request.json['MTRANS']
+        float(request.json['age']),
+        float(request.json['height']),
+        float(request.json['weight']),
+        yesOrNoValues[request.json['family_history']],
+        yesOrNoValues[request.json['favc']],
+        float(request.json['fcvc']),
+        float(request.json['ncp']),
+        float(request.json['caec']),
+        yesOrNoValues[request.json['smoke']],
+        float(request.json['ch2o']),
+        yesOrNoValues[request.json['scc']],
+        float(request.json['faf']),
+        float(request.json['tue']),
+        float(request.json['calc']),
+        mtransValues[request.json['mtrans']]
     ]]))
 
     target_labels = {
-    'Insufficient_Weight': 0,
-    'Normal_Weight': 1,
-    'Overweight_Level_I': 2,
-    'Overweight_Level_II': 3,
-    'Obesity_Type_I': 4,
-    'Obesity_Type_II': 5,
-    'Obesity_Type_III': 6,
-}
+        'Insufficient_Weight': 0,
+        'Normal_Weight': 1,
+        'Overweight_Level_I': 2,
+        'Overweight_Level_II': 3,
+        'Obesity_Type_I': 4,
+        'Obesity_Type_II': 5,
+        'Obesity_Type_III': 6,
+    }
+
     for key, value in target_labels.items():
         if value == pred[0]:
             pred = key
