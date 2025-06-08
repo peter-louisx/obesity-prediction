aab84200dede4cec30f1edaa272363c545d7ed1d
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-08 20:49:38 +0700

    # fix pipeline

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ import joblib
 import numpy as np
 from flask_cors import CORS
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -11,43 +12,39 @@ model = pickle.load(open('model/model.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    caeccalcValues = {
-        'Sometimes': 0.0,
-        'Frequently': 1.0,
-        'Always': 2.0,
-        'no': 3.0, 
-    }
+    try:
+        input_df = pd.DataFrame([{
+            'Age': float(request.json['age']),
+            'Height': float(request.json['height']),
+            'Weight': float(request.json['weight']),
+            'FCVC': float(request.json['fcvc']),
+            'NCP': float(request.json['ncp']),
+            'CH2O': float(request.json['ch2o']),
+            'FAF': float(request.json['faf']),
+            'TUE': float(request.json['tue']),
+            'Gender': request.json['gender'],
+        }])
+        
+        pred = model.predict(input_df)
 
+        target_labels = {
+            'Insufficient_Weight': 0,
+            'Normal_Weight': 1,
+            'Overweight_Level_I': 2,
+            'Overweight_Level_II': 3,
+            'Obesity_Type_I': 4,
+            'Obesity_Type_II': 5,
+            'Obesity_Type_III': 6,
+        }
 
-    pred = model.predict(np.array([[
-        float(request.json['age']),
-        float(request.json['height']),
-        float(request.json['weight']),
-        float(request.json['fcvc']),
-        float(request.json['ncp']),
-        caeccalcValues[request.json['caec']],
-        float(request.json['ch2o']),
-        float(request.json['faf']),
-        float(request.json['tue']),
-        caeccalcValues[request.json['calc']],
-    ]]))
-
-    target_labels = {
-        'Insufficient_Weight': 0,
-        'Normal_Weight': 1,
-        'Overweight_Level_I': 2,
-        'Overweight_Level_II': 3,
-        'Obesity_Type_I': 4,
-        'Obesity_Type_II': 5,
-        'Obesity_Type_III': 6,
-    }
-
-    for key, value in target_labels.items():
-        if value == pred[0]:
-            pred = key
-            break
-    
-    return jsonify({'prediction': pred.replace("_", " ")})
+        for key, value in target_labels.items():
+            if value == pred[0]:
+                pred = key
+                break
+        
+        return jsonify({'prediction': pred.replace("_", " ")})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')  # Runs on http://127.0.0.1:5000 by default
