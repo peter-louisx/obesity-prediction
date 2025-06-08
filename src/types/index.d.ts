30473bce7d9dbccb1d32e47f0d160f28646c2905
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-08 20:23:06 +0700

    # feat finalized pipeline

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,38 +2,38 @@ import { UseFormRegister } from "react-hook-form";
 import { ZodSchema } from "zod";
 
 export default interface ModelAttributes {
-    gender: "Male" | "Female",
-    age: number
-    weight: number,
-    family_history: "yes" | "no",
-    favc: "yes" | "no",
-    fcvc: number,
-    ncp: number,
-    caec: "Always" | "Frequently" | "Sometimes" | "no",
-    smoke: "yes" | "no",
-    ch2o: number,
-    scc: "yes" | "no",
-    faf: number,
-    tue: number,
-    calc: "Always" | "Frequently" | "Sometimes" | "no",
-    mtrans: "Public_Transportation" | "Automobile" | "Walking" | "Motorbike" | "Bike",
+  age: number;
+  weight: number;
+  height: number;
+  fcvc: number;
+  ncp: number;
+  caec: "Always" | "Frequently" | "Sometimes" | "no";
+  ch2o: number;
+  faf: number;
+  tue: number;
+  calc: "Always" | "Frequently" | "Sometimes" | "no";
 }
 
-export type ObesityCategoryType = "Insufficient_Weight" | "Normal_Weight" | "Overweight_Level_I" | "Overweight_Level_II" | "Obesity_Type_I" | "Obesity_Type_II" | "Obesity_Type_III";
+export type ObesityCategoryType =
+  | "Insufficient_Weight"
+  | "Normal_Weight"
+  | "Overweight_Level_I"
+  | "Overweight_Level_II"
+  | "Obesity_Type_I"
+  | "Obesity_Type_II"
+  | "Obesity_Type_III";
 
 export interface FieldType<T extends ZodSchema> {
-    register: UseFormRegister<T>;
-    name: string;
+  register: UseFormRegister<T>;
+  name: string;
 }
 
-export interface NumberFieldType<T extends ZodSchema>
-    extends FieldType<T> {
-        min?: number;
-        max?: number;
-        step?: number;
-    }
+export interface NumberFieldType<T extends ZodSchema> extends FieldType<T> {
+  min?: number;
+  max?: number;
+  step?: number;
+}
 
-export interface DropDownType<T extends ZodSchema>
-    extends FieldType<T> {
-        options: { value: string; label: string | number }[];
-    }
+export interface DropDownType<T extends ZodSchema> extends FieldType<T> {
+  options: { value: string; label: string | number }[];
+}
