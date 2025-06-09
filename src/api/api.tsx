6e08e99c06f4af53c3a3e9de52cc33d4a311567c
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-09 10:39:39 +0700

    # fix remove backend

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import ModelAttributes from "../types";
-const API_URL = process.env.REACT_APP_API_URL;
+
+const API_URL = process.env.API_URL;
 
 export default async function predict(input: ModelAttributes) {
   return await axios.request({
     method: "POST",
-    url: `http://127.0.0.1:5000/api/predict`,
+    url: `${API_URL}/api/predict`,
     data: input,
     headers: {
       Accept: "application/json",
